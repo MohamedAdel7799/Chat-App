86f035625ee34e8c50790feb1071b76a11327d5b
Mohamed Adel <ahmrn1999@gmail.com>
2023-07-04 00:42:02 +0200

    create pages

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,18 +1,31 @@
 import { Socket } from "dgram";
 import {creatServer} from "http";
 import {Server} from "socket.io"
+import { v4 as uuidv4}from"uuid"
 
 const httpServer = creatServer();
 const io = new Server (httpServer,{
     cors:{
 
-        origin:"http://localhost:3000",
+        origin:"http://localhost:3006",
         methods:["Get","Post"],
     }
 });
+io.use((socket,next)=>{
+    const username=socket.handshake.auth.username;
+
+    if(!username){
+        return next(new Error("Invalid username"));
+    }
+    socket.username=username;
+    socket.userid=uuidv4();
+
+    next()
+
+})
 
 io.on("Connection",async(Socket)=>{
-    // socket events
+socket.emit("session",{userid:socket.userid,username:socket.username})
 });
 
 
